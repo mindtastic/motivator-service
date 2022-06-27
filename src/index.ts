d51ed1a913124de4b4798e6c39899f98b264b7be
Johannes Gräger <j.graeger@campus.tu-berlin.de>
2022-06-27 22:11:04 +0200

    Local dev environment

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import log from 'loglevel';
 const app = express();
 const port = process.env.PORT;
 
+const logLevel = process.env.LOG_LEVEL as log.LogLevelDesc || 'warn';
+log.setLevel(logLevel);
+log.info('Initialized logger');
+
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
