a0fb6362d5386a83cef2a6759953136fe826b1bc
Fabian Berndt <f.berndt99@gmail.com>
2022-07-10 18:25:29 +0200

    Fix/moodivator (#20)
    * added url to moodivator
    
    * added empty userid header
    
    * fixed url
    
    * fixed url

--- src/moodivator.js
+++ src/moodivator.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import express from 'express';
+import log from "loglevel";
 
 const moodivatorRouter = express.Router();
 
 moodivatorRouter.get('/', (req, res) => {
-  console.log(req, res);
-
   axios({
-    url: '',
+    url: 'https://mood-diary.mood-diary.svc.cluster.local:80/diary',
     method: 'GET',
     headers: {
-      'X-User-Id': '',
-    },
-  }).then((resp) => console.log(resp));
+      "X-User-Id": ""
+    }
+  }).then((resp) => log.info(resp));
 });
 
 export default moodivatorRouter;
